5623d16a80534cc013461866ff00ed8685fc6440
Gyeongjin Lee <asdfg031897@gmail.com>
2023-11-24 21:19:18 -0800

    update some background images

--- js/background.js
+++ js/background.js
@@ -1,12 +1,13 @@
 // const backgroundContainer = document.querySelector(".background-container");
 
 const images = [
-  "01.jpeg",
-  "02.jpeg",
+  "01.jpg",
+  "02.jpg",
   "03.jpeg",
-  "04.jpeg",
+  "04.png",
   "05.jpg",
-  "06.jpeg",
+  "06.webp",
+  "maybe.png",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
@@ -18,7 +19,8 @@ bgImage.src = `img/${chosenImage}`;
 // document.body.appendChild(bgImage);
 
 document.body.style.backgroundImage = `url(${bgImage.src})`;
-
+document.body.style.backgroundSize = "100% 100%";
+document.body.style.backgroundRepeat = "no-repeat";
 //add class name if i want
 // document.body.classList.add("custom-background-class");
 
